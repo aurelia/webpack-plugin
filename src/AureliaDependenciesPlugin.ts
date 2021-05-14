10bcaf0c28f0e7d656c0e43dd114e5c426135d29
bigopon <bigopon.777@gmail.com>
2021-05-14 22:20:32 +1000

    chore: simplify diff in AureliaDependenciesPlugin

--- src/AureliaDependenciesPlugin.ts
+++ src/AureliaDependenciesPlugin.ts
@@ -6,35 +6,6 @@ import { BasicEvaluatedExpression as $BasicEvaluatedExpression, DependencyOption
 const BasicEvaluatedExpression: $BasicEvaluatedExpression = require("webpack/lib/javascript/BasicEvaluatedExpression");
 const TAP_NAME = "Aurelia:Dependencies";
 
-export class AureliaDependenciesPlugin {
-  private parserPlugin: ParserPlugin;
-
-  constructor(...methods: string[]) {
-    // Always include PLATFORM.moduleName as it's what used in libs.
-    if (!methods.includes("PLATFORM.moduleName")) {
-      methods.push("PLATFORM.moduleName");
-    }
-    this.parserPlugin = new ParserPlugin(methods);
-  }
-
-  apply(compiler: webpack.Compiler) {
-    compiler.hooks.compilation.tap(TAP_NAME, (compilation, params) => {
-      const normalModuleFactory = params.normalModuleFactory;
-
-      compilation.dependencyFactories.set(AureliaDependency, normalModuleFactory);
-      compilation.dependencyTemplates.set(AureliaDependency, new Template());
-
-      const handler = (parser: webpack.javascript.JavascriptParser) => {
-        this.parserPlugin.apply(parser);
-      }
-
-      normalModuleFactory.hooks.parser.for("javascript/dynamic").tap(TAP_NAME, handler);
-      normalModuleFactory.hooks.parser.for("javascript/auto").tap(TAP_NAME, handler);
-      normalModuleFactory.hooks.parser.for("javascript/esm").tap(TAP_NAME, handler);
-    });
-  }
-}
-
 class AureliaDependency extends IncludeDependency {
   constructor(request: string, 
               public range: [number, number], 
@@ -55,7 +26,7 @@ class ParserPlugin {
 
   apply(parser: webpack.javascript.JavascriptParser) {
 
-    const addDependency = (module: string, range: [number, number], options?: DependencyOptions) => {
+    function addDependency(module: string, range: [number, number], options?: DependencyOptions) {
       let dep = new AureliaDependency(module, range, options);
       parser.state.current.addDependency(dep);
       return true;
@@ -85,63 +56,94 @@ class ParserPlugin {
       return undefined;
     });
 
-    hooks.call.for('PLATFORM.moduleName').tap(TAP_NAME, (expr: estree.CallExpression) => {
-      if (expr.arguments.length === 0 || expr.arguments.length > 2) {
-        return;
-      }
+    for (let method of this.methods) {
+      hooks.call.for(method).tap(TAP_NAME, (expr: estree.CallExpression) => {
+        if (expr.arguments.length === 0 || expr.arguments.length > 2) {
+          return;
+        }
 
-      let [arg1, arg2] = expr.arguments as estree.Expression[];
-      let param1 = parser.evaluateExpression(arg1);
-      if (!param1?.isString())
-        return;
-      if (expr.arguments.length === 1) {
-        // Normal module dependency
-        // PLATFORM.moduleName('some-module')
-        return addDependency(param1.string!, expr.range!);
-      }
+        let [arg1, arg2] = expr.arguments as estree.Expression[];
+        let param1 = parser.evaluateExpression(arg1);
+        if (!param1?.isString())
+          return;
+        if (expr.arguments.length === 1) {
+          // Normal module dependency
+          // PLATFORM.moduleName('some-module')
+          return addDependency(param1.string!, expr.range!);
+        }
 
-      let options: DependencyOptions | undefined;
-      let param2 = parser.evaluateExpression(arg2)!;
-      if (param2?.isString()) {
-        // Async module dependency
-        // PLATFORM.moduleName('some-module', 'chunk name');
-        options = { chunk: param2.string };
-      }
-      else if (arg2.type === "ObjectExpression") {
-        // Module dependency with extended options
-        // PLATFORM.moduleName('some-module', { option: value });
-        options = {};
-        // NOTE:
-        // casting here is likely to be correct, as we can declare the following not supported:
-        // PLATFORM.moduleName('some-module', { ...options })
-        for (let prop of arg2.properties) {
-          if (prop.type !== 'Property'
-            || prop.method
-            // theoretically, PLATFORM.moduleName('..', { ['chunk']: '' })
-            // works, but ... not a lot of sense supporting it
-            || prop.computed
-            || prop.key.type !== "Identifier"
-          )
-            continue;
+        let options: DependencyOptions | undefined;
+        let param2 = parser.evaluateExpression(arg2)!;
+        if (param2?.isString()) {
+          // Async module dependency
+          // PLATFORM.moduleName('some-module', 'chunk name');
+          options = { chunk: param2.string };
+        }
+        else if (arg2.type === "ObjectExpression") {
+          // Module dependency with extended options
+          // PLATFORM.moduleName('some-module', { option: value });
+          options = {};
+          // NOTE:
+          // casting here is likely to be correct, as we can declare the following not supported:
+          // PLATFORM.moduleName('some-module', { ...options })
+          for (let prop of arg2.properties) {
+            if (prop.type !== 'Property'
+              || prop.method
+              // theoretically, PLATFORM.moduleName('..', { ['chunk']: '' })
+              // works, but ... not a lot of sense supporting it
+              || prop.computed
+              || prop.key.type !== "Identifier"
+            )
+              continue;
 
-          let value = parser.evaluateExpression(prop.value as estree.Literal);
-          switch (prop.key.name) {
-            case "chunk":
-              if (value?.isString()) 
-                options.chunk = value.string;
-              break;
-            case "exports":
-              if (value?.isArray() && value.items!.every(v => v.isString()))
-                options.exports = value.items!.map(v => v.string!);
-              break;
+            let value = parser.evaluateExpression(prop.value as estree.Literal);
+            switch (prop.key.name) {
+              case "chunk":
+                if (value?.isString()) 
+                  options.chunk = value.string;
+                break;
+              case "exports":
+                if (value?.isArray() && value.items!.every(v => v.isString()))
+                  options.exports = value.items!.map(v => v.string!);
+                break;
+            }
           }
         }
+        else {
+          // Unknown PLATFORM.moduleName() signature
+          return;
+        }
+        return addDependency(param1.string!, expr.range!, options);
+      });
+    }
+  }
+}
+
+export class AureliaDependenciesPlugin {
+  private parserPlugin: ParserPlugin;
+
+  constructor(...methods: string[]) {
+    // Always include PLATFORM.moduleName as it's what used in libs.
+    if (!methods.includes("PLATFORM.moduleName")) {
+      methods.push("PLATFORM.moduleName");
+    }
+    this.parserPlugin = new ParserPlugin(methods);
+  }
+
+  apply(compiler: webpack.Compiler) {
+    compiler.hooks.compilation.tap(TAP_NAME, (compilation, params) => {
+      const normalModuleFactory = params.normalModuleFactory;
+
+      compilation.dependencyFactories.set(AureliaDependency, normalModuleFactory);
+      compilation.dependencyTemplates.set(AureliaDependency, new Template());
+
+      const handler = (parser: webpack.javascript.JavascriptParser) => {
+        this.parserPlugin.apply(parser);
       }
-      else {
-        // Unknown PLATFORM.moduleName() signature
-        return;
-      }
-      return addDependency(param1.string!, expr.range!, options);
+
+      normalModuleFactory.hooks.parser.for("javascript/dynamic").tap(TAP_NAME, handler);
+      normalModuleFactory.hooks.parser.for("javascript/auto").tap(TAP_NAME, handler);
+      normalModuleFactory.hooks.parser.for("javascript/esm").tap(TAP_NAME, handler);
     });
   }
 }
